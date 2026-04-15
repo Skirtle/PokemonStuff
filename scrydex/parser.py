8aa7c8ffe5f3070aa649b264d5ea4f8942e87378
Dalton Kajander <daltonkajander@yahoo.com>
2026-04-15 17:09:27 -0400

    reset parser

--- scrydex/parser.py
+++ scrydex/parser.py
@@ -1,35 +1,10 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import Any
-from abc import ABC
+from abc import ABC, abstractmethod
 
 import lexer
 
-@dataclass
-class Node(ABC): pass
-
-@dataclass
-class BoolNode(Node):
-    left: Node | None
-    right: Node | None
-
-@dataclass
-class AttributeNode(Node):
-    value: Any = None
-
-@dataclass
-class UnaryNode(Node):
-    value: Any = None
-
-def token_to_node(token: tuple[str, str, str] | tuple[str, str]) -> Node:
-    return Node()
-
-def create_ast(head: Node) -> Node:
-    # Base case
-    if (isinstance(head, AttributeNode)):
-        ... 
-
-    return Node()
     
 if __name__ == "__main__":
     query = "-(t:fire atk>50) or (spd>100 -(t:ghost t:fire))"
@@ -38,6 +13,4 @@ if __name__ == "__main__":
     print(query)
     for token in classified_tokens:
         print(f"\t{token}")
-    
-    ast = create_ast(classified_tokens)
-    print(ast)
+        
