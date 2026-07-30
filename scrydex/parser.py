c13222946a321fc93df41de41ac2aced1e0a437d
Dalton Kajander <daltonkajander@yahoo.com>
2026-07-30 14:18:11 -0400

    created scrytree
    scrytree is the ast for scrydex

--- scrydex/parser.py
+++ scrydex/parser.py
@@ -1,20 +1,6 @@
-from __future__ import annotations
-from dataclasses import dataclass, field
-from typing import Any
-from abc import ABC, abstractmethod
-
 import lexer
 
-@dataclass
-class Node(ABC):
-    type: str
 
-    @abstractmethod
-    def evaluate(self): pass
-
-@dataclass
-class UnaryNode(Node):
-    ...
     
 if __name__ == "__main__":
     query = "-(t:fire atk>50) or (spd>100 -(t:ghost t:fire))"
---
+++ scrydex/scrytree.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from typing import Any
+from abc import ABC, abstractmethod
+
+@dataclass
+class Expression(ABC):
+    @abstractmethod
+    def evaluate(self): 
+        raise NotImplementedError
+
+@dataclass
+class Literal(Expression):
+    value: int | str
+    
+    def evaluate(self) -> Any:
+        return self.value
+    
+@dataclass
+class BinaryOperator(Expression):
+    left: Expression
+    op: str
+    right: Expression
+    
+    def evaluate(self) -> Any:
+        left_value = self.left.evaluate()
+        right_value = self.right.evaluate()
+        
+        if (self.op == "+"): return left_value + right_value
+        elif (self.op == "*"): return left_value * right_value
+        raise ValueError(f"Unsupported operator {self.op}")
+    
+@dataclass
+class UnaryOperator(Expression):
+    op: str
+    value: Expression
+    
+    
+if __name__ == "__main__":
+    tree = BinaryOperator(BinaryOperator(Literal(5), "+", Literal(7)), "*", BinaryOperator(Literal(2), "*", Literal(7)))
+    print(tree.evaluate())
